f8806b18576b2d4e9821611e523bc67a1406c682
ademar <ademar.lisboa@gmail.com>
2025-11-25 10:37:26 -0300

    operadores 2

--- primeiro_programa.py
+++ primeiro_programa.py
@@ -1,3 +1,9 @@
+
+produto1 = 10
+produto2 = 20
+
+print (produto1 + produto2)
+
 print (1 + 1)
 
 print (10 - 2)
@@ -11,3 +17,14 @@ print (10 % 3)
 #exponenciação
 
 print (10 ** 3)
+
+#precedencia operadores 
+
+print (10 - 5 * 2)
+
+print ((10 - 5) * 2)
+
+print (10 ** 2  * 2 )
+
+print (10 / 2  * 4 )
+
